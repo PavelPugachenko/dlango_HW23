959b7cc2bb408f08925a77a0a9a34618bb5cfbc7
pavel <pugachenkopavel72@gmail.com>
2024-12-22 17:14:13 +0500

    3

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ('name', 'description',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
