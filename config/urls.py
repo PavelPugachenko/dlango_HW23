f3996a22a7702caaa5d90c4157adcaeae0176872
pavel <pugachenkopavel72@gmail.com>
2024-12-28 17:48:34 +0500

    ufffг12

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog'))
+    path('catalog/', include('catalog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
