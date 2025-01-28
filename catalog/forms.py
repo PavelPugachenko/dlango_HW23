e63a7b6a225b33dab98aa661b41fa3cc4375d03a
pavel <pugachenkopavel72@gmail.com>
2025-01-28 18:48:49 +0500

    commit_hw_26

--- catalog/forms.py
+++ catalog/forms.py
@@ -42,8 +42,17 @@ class ProductForm(forms.ModelForm):
         return price
 
 
-
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['name', 'description']
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
