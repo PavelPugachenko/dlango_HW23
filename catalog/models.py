639813d6cc1ad5f59521e928c715d027ae4ff74c
pavel <pugachenkopavel72@gmail.com>
2025-02-19 16:33:28 +0500

    28

--- catalog/models.py
+++ catalog/models.py
@@ -32,7 +32,7 @@ class Product(models.Model):
     created_at = models.DateField(verbose_name="Дата создания", blank=True, null=True)
     updated_at = models.DateField(verbose_name="Дата последнего изменения", blank=True, null=True)
     is_published = models.BooleanField(default=False)
-    owner = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    owner = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
